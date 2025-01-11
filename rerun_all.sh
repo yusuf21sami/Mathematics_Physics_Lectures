84f72b7bf51ef9bc1ba6caabdbd6906822c028ac
dchorazkiewicz <damian.ch.datascience@gmail.com>
2025-01-11 21:50:35 +0100

    feat: add script to render Discrete Mathematics lecture notes and create symbolic links

--- rerun_all.sh
+++ rerun_all.sh
@@ -104,6 +104,48 @@ ln -sf ../../Lecture_Notes/Mathematics/Calculus.html Calculus.html
 # Powrót do katalogu głównego
 cd "$ROOT_DIR" || exit
 
+## RERUN Discrete Mathematics Lecture Notes
+
+# Directory containing the .qmd files
+TARGET_DIR="Lecture_Notes/Discrete_Mathematics"
+
+# Check if the directory exists
+if [ ! -d "$TARGET_DIR" ]; then
+  echo "Error: Directory $TARGET_DIR does not exist."
+  exit 1
+fi
+
+# Change to the target directory
+cd "$TARGET_DIR" || exit
+
+# Iterate over all .qmd files in the directory
+for qmd_file in *.qmd; do
+  # Check if there are any .qmd files
+  if [ -e "$qmd_file" ]; then
+    echo "Rendering $qmd_file..."
+    quarto render "$qmd_file"
+  else
+    echo "No .qmd files found in $TARGET_DIR."
+    exit 0
+  fi
+done
+
+# Change back to the original directory
+cd ../../
+
+# Automatyczne ustawienie katalogu głównego na bieżący katalog
+ROOT_DIR=$(pwd)
+
+# print the current directory
+echo "Current directory: $ROOT_DIR"
+
+# Tworzenie linków w katalogu Discrete_Mathematics
+cd "$ROOT_DIR/docs/Discrete_Mathematics" || exit
+
+ln -sf ../../Lecture_Notes/Discrete_Mathematics/Discrete_Mathematics.html Discrete_Mathematics.html
+
+# Powrót do katalogu głównego
+cd "$ROOT_DIR" || exit
 
 
 # Deploy using mkdocs
